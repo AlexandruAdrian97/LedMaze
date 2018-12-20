e4897eb63a3e7e44cdb4d4f701cc7ac547dc33bb
AlexandruAdrian <adi.alex06@gmail.com>
2018-12-20 15:12:55 +0200

    Add 5 more levels and score with High Score

--- LedMaze/LedMaze.ino
+++ LedMaze/LedMaze.ino
@@ -14,6 +14,8 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+const int ii = 8, jj = 8;
+
 
 #define JOY_X A0
 #define JOY_Y A1
@@ -30,10 +32,10 @@ int j;
 
 int level = 0;
 int lives = 5;
-long score = 0;
+int highScore = 1;
+int scoreLives = 20;
 
-
-int mazeOne[8][8] = {
+bool mazeOne[ii][jj] = {
   {0,0,1,1,1,1,1,1},
   {0,0,0,0,0,1,1,1},
   {1,1,1,0,0,1,1,1},
@@ -44,7 +46,7 @@ int mazeOne[8][8] = {
   {1,1,1,1,1,1,0,0},
 };
 
-int mazeTwo[8][8] = {
+bool mazeTwo[ii][jj] = {
   {0,0,1,1,1,1,1,1},
   {1,0,0,0,0,0,1,1},
   {1,1,1,1,1,0,1,1},
@@ -55,7 +57,8 @@ int mazeTwo[8][8] = {
   {1,1,1,1,1,1,0,0},
 };
 
-int mazeThree[8][8] = {
+
+bool mazeThree[ii][jj] = {
   {0,0,0,0,1,0,0,0},
   {0,0,1,0,1,0,1,0},
   {1,1,1,0,0,0,1,0},
@@ -66,7 +69,74 @@ int mazeThree[8][8] = {
   {0,0,0,0,0,0,0,0},
 };
 
-int gameOverM[8][8] = {
+
+bool mazeFour[ii][jj] = {
+  {0,0,1,1,1,1,1,1},
+  {0,0,1,1,1,0,0,0},
+  {1,0,1,1,1,0,1,0},
+  {1,0,1,0,0,0,1,0},
+  {1,0,1,0,1,1,1,0},
+  {1,0,1,0,1,0,0,0},
+  {1,0,1,0,1,0,1,1},
+  {1,0,0,0,1,0,0,0},
+};
+
+bool mazeFive[ii][jj] = {
+  {0,1,1,0,0,0,0,1},
+  {0,0,1,0,1,1,0,1},
+  {1,0,0,0,1,1,0,1},
+  {1,1,1,1,1,1,0,1},
+  {0,0,0,0,0,1,0,1},
+  {0,1,1,1,0,0,0,1},
+  {0,1,1,0,1,1,1,1},
+  {0,0,0,0,0,0,0,0},
+};
+
+bool mazeSix[ii][jj] = {
+  {0,1,1,1,1,1,1,1},
+  {0,1,0,0,0,0,0,0},
+  {0,0,0,1,1,1,1,0},
+  {1,1,1,1,0,0,0,0},
+  {1,0,0,0,0,1,1,1},
+  {1,0,1,1,1,1,1,1},
+  {1,0,0,1,0,0,0,1},
+  {1,1,0,0,0,1,0,0},
+};
+
+bool mazeSeven[ii][jj] = {
+  {0,0,1,1,1,0,0,1},
+  {1,0,0,1,0,0,0,1},
+  {1,1,0,0,0,1,0,1},
+  {1,1,1,1,1,0,0,1},
+  {1,0,0,0,1,0,1,1},
+  {0,0,1,0,0,0,1,1},
+  {0,1,1,1,1,1,1,1},
+  {0,0,0,0,0,0,0,0},
+};
+
+bool mazeEight[ii][jj] = {
+  {0,1,1,0,0,0,0,0},
+  {0,1,1,0,1,1,1,0},
+  {0,0,0,0,1,0,0,0},
+  {1,1,1,1,1,0,1,1},
+  {0,0,0,1,0,0,1,1},
+  {0,1,0,0,0,1,1,1},
+  {0,1,1,1,1,1,1,1},
+  {0,0,0,0,0,0,0,0},
+};
+
+bool mazeNine[ii][jj] = {
+  {0,0,1,0,0,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,0,0,1,0,1,0},
+  {1,1,1,1,1,0,1,0},
+  {0,0,0,0,0,0,1,0},
+  {0,1,1,1,1,1,1,0},
+  {0,1,0,0,0,1,0,0},
+  {0,0,0,1,0,0,0,0},
+};
+
+bool gameOverM[ii][jj] = {
   {1,0,0,0,0,0,0,1},
   {0,1,0,0,0,0,1,0},
   {0,0,1,0,0,1,0,0},
@@ -77,7 +147,7 @@ int gameOverM[8][8] = {
   {1,0,0,0,0,0,0,1},
 };
 
-int finishM[8][8] = {
+bool finishM[ii][jj] = {
   {0,0,1,0,0,0,0,0},
   {0,0,1,0,0,0,0,0},
   {1,1,1,1,1,0,0,0},
@@ -88,7 +158,7 @@ int finishM[8][8] = {
   {0,0,0,0,1,0,0,0},
 };
 
-int transitionLevelOne[8][8] = {
+bool transitionLevelOne[ii][jj] = {
   {1,0,0,0,0,0,1,0},
   {1,0,1,0,1,0,1,0},
   {1,0,1,0,1,0,1,0},
@@ -99,7 +169,7 @@ int transitionLevelOne[8][8] = {
   {0,0,0,0,0,1,0,0},
 };
 
-int transitionLevelTwo[8][8] = {
+bool transitionLevelTwo[ii][jj] = {
   {1,0,0,0,0,0,1,0},
   {1,0,1,0,1,0,1,0},
   {1,0,1,0,1,0,1,0},
@@ -110,7 +180,7 @@ int transitionLevelTwo[8][8] = {
   {0,0,0,0,1,0,1,0},
 };
 
-int transitionLevelThree[8][8] = {
+bool transitionLevelThree[ii][jj] = {
   {1,0,0,0,0,0,1,0},
   {1,0,1,0,1,0,1,0},
   {1,0,1,0,1,0,1,0},
@@ -121,7 +191,62 @@ int transitionLevelThree[8][8] = {
   {0,0,0,1,0,1,0,1},
 };
 
-int standByM[8][8] = {
+bool transitionLevelFour[ii][jj] = {
+  {1,0,0,0,0,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,0,1,0,0,1,0},
+  {0,0,0,0,0,0,0,0},
+  {0,1,0,1,0,0,0,1},
+  {0,1,0,0,1,0,1,0},
+  {0,1,0,0,0,1,0,0},
+};
+
+bool transitionLevelFive[ii][jj] = {
+  {1,0,0,0,0,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,0,1,0,0,1,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,1,0,0,0,1},
+  {0,0,0,0,1,0,1,0},
+  {0,0,0,0,0,1,0,0},
+};
+
+bool transitionLevelSix[ii][jj] = {
+  {1,0,0,0,0,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,0,1,0,0,1,0},
+  {0,0,0,0,0,0,0,0},
+  {0,1,0,1,0,1,0,0},
+  {0,1,0,1,0,1,0,0},
+  {0,0,1,0,0,1,0,0},
+};
+
+bool transitionLevelSeven[ii][jj] = {
+  {1,0,0,0,0,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,0,1,0,0,1,0},
+  {0,0,0,0,0,0,0,0},
+  {0,1,0,1,0,1,1,0},
+  {0,1,0,1,0,1,1,0},
+  {0,0,1,0,0,1,1,0},
+};
+
+bool transitionLevelEight[ii][jj] = {
+  {1,0,0,0,0,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,1,0,1,0,1,0},
+  {1,0,0,1,0,0,1,0},
+  {0,0,0,0,0,0,0,0},
+  {1,0,1,0,1,1,1,0},
+  {1,0,1,0,1,1,1,0},
+  {0,1,0,0,1,1,1,0},
+};
+
+bool standByM[ii][jj] = {
   {0,1,0,1,1,0,1,0},
   {1,0,1,0,0,1,0,1},
   {0,1,0,1,1,0,1,0},
@@ -131,6 +256,7 @@ int standByM[8][8] = {
   {1,0,1,0,0,1,0,1},
   {0,1,0,1,1,0,1,0},
 };
+
 
 
 void displayLives(int dsplay)
@@ -141,14 +267,19 @@ void displayLives(int dsplay)
 	lcd.print(dsplay);
 }
 
-void scoreTime(long t)
+void score(int t, int v)
 {
 	lcd.begin(16, 2);
-	lcd.setCursor(4, 0);
-	lcd.print("Scoretime: ");
-	lcd.setCursor(5, 1);
-	lcd.print(t / 1000);
-	lcd.print(" sec")
+	lcd.setCursor(0, 0);
+	lcd.print("Score: ");
+	lcd.setCursor(7, 0);
+	lcd.print(t * 6);
+	lcd.print(" pts");
+	lcd.setCursor(0, 1);
+	lcd.print("H. Scr: ");
+	lcd.setCursor(7, 1);
+	lcd.print(v * 6);
+	lcd.print(" pts");
 }
 
 void movement()
@@ -161,94 +292,145 @@ void movement()
 
 	if (xValue > 600)
 	{
-		if (presentX == 7)
-		{
-			presentX = 7;
-		}
-		else
-		{
+		if (presentX != 7)
 			presentX++;
-		}
 	}
 	else
-		if (xValue < 400)
+		if (xValue < 300)
 		{
-			if (presentX == 0)
-			{
-				presentX = 0;
-			}
-			else presentX--;
+			if (presentX != 0)
+				presentX--;
 		}
 	if (yValue > 600)
 	{
-		if (presentY == 7)
-		{
-			presentY = 7;
-		}
-		else
-		{
+		if (presentY != 7)
 			presentY++;
-		}
 	}
 	else
-		if (yValue < 400)
+		if (yValue < 300)
 		{
-			if (presentY == 0)
-			{
-				presentY = 0;
-			}
-			else presentY--;
+			if (presentY != 0)
+				presentY--;
 		}
-	delay(100);
+	delay(30);
 }
 
-void levelOne()
+int levelOne()
 {
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
 			lc.setLed(0, i, j, mazeOne[i][j]);
 }
 
 void levelTwo()
 {
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
 			lc.setLed(0, i, j, mazeTwo[i][j]);
 }
 
 void levelThree()
 {
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
 			lc.setLed(0, i, j, mazeThree[i][j]);
+}
+
+void levelFour()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, mazeFour[i][j]);
+}
+
+void levelFive()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, mazeFive[i][j]);
+}
+
+void levelSix()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, mazeSix[i][j]);
+}
+
+void levelSeven()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, mazeSeven[i][j]);
+}
+
+void levelEight()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, mazeEight[i][j]);
 }
 
 void standBy()
 {
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
 			lc.setLed(0, i, j, standByM[i][j]);
 }
 
 void transitionOne()
 {
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
 			lc.setLed(0, i, j, transitionLevelOne[i][j]);
 }
 
 void transitionTwo()
 {
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
 			lc.setLed(0, i, j, transitionLevelTwo[i][j]);
 }
 
 void transitionThree()
 {
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
 			lc.setLed(0, i, j, transitionLevelThree[i][j]);
+}
+
+void transitionFour()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, transitionLevelFour[i][j]);
+}
+
+void transitionFive()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, transitionLevelFive[i][j]);
+}
+
+void transitionSix()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, transitionLevelSix[i][j]);
+}
+
+void transitionSeven()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, transitionLevelSeven[i][j]);
+}
+void transitionEight()
+{
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			lc.setLed(0, i, j, transitionLevelEight[i][j]);
 }
 
 void gameOver()
@@ -294,11 +476,10 @@ void levelSwitch()
 		break;
 
 	case 1:
+		lc.clearDisplay(0);
 		transitionOne();
-		delay(2000);
+		delay(500); // for the transition between the levels;
 		level++;
-		score = 0;
-		score = millis();
 		break;
 
 	case 2:
@@ -308,29 +489,34 @@ void levelSwitch()
 		if (mazeOne[presentY][presentX] == true)
 		{
 			lives = lives - 1;
+			scoreLives = scoreLives - 2;
 			presentY = 0;
 			presentX = 0;
 		}
 
 		lc.setLed(0, previousY, previousX, 0);
+		delay(45);
 		lc.setLed(0, presentY, presentX, 1);
 
 		if (lives == 0)
 		{
-			level = 8;
+			level = 18;
+			scoreLives = scoreLives * 2;
 			break;
 		}
 
-		lc.setLed(0, previousY, previousX, 0);
-		lc.setLed(0, presentY, presentX, 1);
-
 		if (presentY == 7 && presentX == 7)
+		{
 			level++;
+			scoreLives = scoreLives * 3;
+			presentX = 0;
+			presentY = 0;
+		}
 		break;
 
 	case 3:
 		transitionTwo();
-		delay(2000);
+		delay(500);
 		level++;
 		break;
 
@@ -341,29 +527,34 @@ void levelSwitch()
 		if (mazeTwo[presentY][presentX] == true)
 		{
 			lives = lives - 1;
-			presentY = 7;
-			presentX = 7;
+			scoreLives = scoreLives - 2;
+			presentY = 0;
+			presentX = 0;
 		}
 
 		lc.setLed(0, previousY, previousX, 0);
+		delay(45);
 		lc.setLed(0, presentY, presentX, 1);
 
 		if (lives == 0)
 		{
-			level = 8;
+			level = 18;
+			scoreLives = scoreLives * 4;
 			break;
 		}
 
-		lc.setLed(0, previousY, previousX, 0);
-		lc.setLed(0, presentY, presentX, 1);
-
-		if (presentY == 0 && presentX == 0)
+		if (presentY == 7 && presentX == 7)
+		{
 			level++;
+			scoreLives = scoreLives * 5;
+			presentX = 0;
+			presentY = 0;
+		}
 		break;
 
 	case 5:
 		transitionThree();
-		delay(2000);
+		delay(500);
 		level++;
 		break;
 
@@ -374,48 +565,255 @@ void levelSwitch()
 		if (mazeThree[presentY][presentX] == true)
 		{
 			lives = lives - 1;
+			scoreLives = scoreLives - 2;
 			presentY = 0;
 			presentX = 0;
 		}
 
 		lc.setLed(0, previousY, previousX, 0);
+		delay(45);
 		lc.setLed(0, presentY, presentX, 1);
 
 		if (lives == 0)
 		{
-			level = 8;
+			level = 18;
+			scoreLives = scoreLives * 6;
 			break;
 		}
 
-		lc.setLed(0, previousY, previousX, 0);
-		lc.setLed(0, presentY, presentX, 1);
-
 		if (presentY == 7 && presentX == 7)
+		{
 			level++;
+			scoreLives = scoreLives * 7;
+			presentX = 0;
+			presentY = 0;
+		}
 		break;
 
 	case 7:
-		lc.clearDisplay(0);
-		finish();
-		delay(1500);
-		scoreTime(score);
-		delay(1500);
-		level = 0;
-		lives = 5;
-		presentX = 0;
-		presentY = 0;
+		transitionFour();
+		delay(500);
+		level++;
 		break;
 
 	case 8:
+		displayLives(lives);
+		levelFour();
+
+		if (mazeFour[presentY][presentX] == true)
+		{
+			lives = lives - 1;
+			scoreLives = scoreLives - 2;
+			presentY = 0;
+			presentX = 0;
+		}
+
+		lc.setLed(0, previousY, previousX, 0);
+		delay(45);
+		lc.setLed(0, presentY, presentX, 1);
+
+		if (lives == 0)
+		{
+			level = 18;
+			scoreLives = scoreLives * 8;
+			break;
+		}
+
+		if (presentY == 7 && presentX == 7)
+		{
+			level++;
+			scoreLives = scoreLives * 9;
+			presentX = 0;
+			presentY = 0;
+		}
+		break;
+
+	case 9:
+		transitionFive();
+		delay(500);
+		level++;
+		break;
+
+	case 10:
+		displayLives(lives);
+		levelFive();
+
+		if (mazeFive[presentY][presentX] == true)
+		{
+			lives = lives - 1;
+			scoreLives = scoreLives - 2;
+			presentY = 0;
+			presentX = 0;
+		}
+
+		lc.setLed(0, previousY, previousX, 0);
+		delay(45);
+		lc.setLed(0, presentY, presentX, 1);
+
+		if (lives == 0)
+		{
+			level = 18;
+			scoreLives = scoreLives * 10;
+			break;
+		}
+
+		if (presentY == 7 && presentX == 7)
+		{
+			level++;
+			scoreLives = scoreLives * 11;
+			presentX = 0;
+			presentY = 0;
+		}
+		break;
+
+	case 11:
 		lc.clearDisplay(0);
-		gameOver();
-		delay(1500);
-		scoreTime(score);
-		delay(1500);
+		transitionSix();
+		delay(500); // for the transition between the levels;
+		level++;
+		break;
+
+	case 12:
+		displayLives(lives);
+		levelSix();
+
+		if (mazeSix[presentY][presentX] == true)
+		{
+			lives = lives - 1;
+			scoreLives = scoreLives - 2;
+			presentY = 0;
+			presentX = 0;
+		}
+
+		lc.setLed(0, previousY, previousX, 0);
+		delay(45);
+		lc.setLed(0, presentY, presentX, 1);
+
+		if (lives == 0)
+		{
+			level = 18;
+			scoreLives = scoreLives * 12;
+			break;
+		}
+
+		if (presentY == 7 && presentX == 7)
+
+		{
+			level++;
+			scoreLives = scoreLives * 13;
+			presentX = 0;
+			presentY = 0;
+		}
+		break;
+
+	case 13:
+		transitionSeven();
+		delay(500);
+		level++;
+		break;
+
+	case 14:
+		displayLives(lives);
+		levelSeven();
+
+		if (mazeSeven[presentY][presentX] == true)
+		{
+			lives = lives - 1;
+			scoreLives = scoreLives - 2;
+			presentY = 0;
+			presentX = 0;
+		}
+
+		lc.setLed(0, previousY, previousX, 0);
+		delay(45);
+		lc.setLed(0, presentY, presentX, 1);
+
+		if (lives == 0)
+		{
+			level = 18;
+			scoreLives = scoreLives * 14;
+			break;
+		}
+
+		if (presentY == 7 && presentX == 7)
+		{
+			level++;
+			scoreLives = scoreLives * 15;
+			presentX = 0;
+			presentY = 0;
+		}
+		break;
+
+	case 15:
+		transitionEight();
+		delay(500);
+		level++;
+		break;
+
+	case 16:
+		displayLives(lives);
+		levelEight();
+
+		if (mazeEight[presentY][presentX] == true)
+		{
+			lives = lives - 1;
+			scoreLives = scoreLives - 2;
+			presentY = 0;
+			presentX = 0;
+		}
+
+		lc.setLed(0, previousY, previousX, 0);
+		delay(45);
+		lc.setLed(0, presentY, presentX, 1);
+
+		if (lives == 0)
+		{
+			level = 18;
+			scoreLives = scoreLives * 16;
+			break;
+		}
+
+		if (presentY == 7 && presentX == 7)
+		{
+			level++;
+			scoreLives = scoreLives * 17;
+			presentX = 0;
+			presentY = 0;
+		}
+		break;
+
+	case 17:
+		lc.clearDisplay(0);
+		finish();
+		if (scoreLives > highScore)
+		{
+			highScore = scoreLives;
+		}
+		delay(2000);
+		score(scoreLives, highScore);
+		delay(2000);
 		level = 0;
 		lives = 5;
 		presentX = 0;
 		presentY = 0;
+		scoreLives = 20;
+		break;
+
+	case 18:
+		lc.clearDisplay(0);
+		gameOver();
+		if (scoreLives > highScore)
+		{
+			highScore = scoreLives;
+		}
+		delay(2000);
+		score(scoreLives, highScore);
+		delay(2000);
+		level = 0;
+		lives = 5;
+		presentX = 0;
+		presentY = 0;
+		scoreLives = 20;
 		break;
 	}
 }
@@ -428,7 +826,7 @@ void setup()
 	lc.clearDisplay(0);
 
 	pinMode(button, INPUT_PULLUP);
-	pinMode(V0, OUTPUT); // PWM instead of POTENTIOMETRE
+	pinMode(V0, OUTPUT); //PWM instead of POTENTIOMETRE
 	analogWrite(V0, 100);
 }
 
